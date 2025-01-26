206f7ab2bade1f9a157c4f6ce9c8860de3232a49
Habib Ritonga <128818642+habibdefunc@users.noreply.github.com>
2025-01-26 23:26:25 +0700

    Create form Login (#4)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/dashboard";
+import LoginComp from "../components/utils/login";
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginComp />} />
       </Routes>
     </Router>
   );
